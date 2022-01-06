5e29cb7a232b11f2bdb56486a43c2160401c37b7
RoboKamu <91156959+RoboKamu@users.noreply.github.com>
2022-01-06 22:43:23 +0100

    Update clock.py

--- clock.py
+++ clock.py
@@ -1,51 +1,49 @@
 import RPi.GPIO as gpio
 from time import sleep
 
-#setup gpio pins 
+# setup gpio pins
 gpio.setmode(gpio.BOARD)
 pins = [7, 8, 11, 26, 40]
 for pin in pins:
-	gpio.setup(pin, gpio.OUT)
+    gpio.setup(pin, gpio.OUT)
 
-#make a class that has an object for each number (0, 1, 2 etc)
+
+# make a class that has an object for each number (0, 1, 2 etc)
 class Count:
-	def __init__(self, num):
-		self.num = num
-		self.countList = []
-		self.leftLED = 7
-		self.rightLED = 8
-		self.topLED = 11
-		self.midLED = 26
-		self.botLED = 40
-		
-	def numZero():
-		#list of all lines of LED used to make zero
-		LEDList = [leftLED, rightLED, topLED, botLED]
-		
-		for LED in LEDList:
-			gpio.output(LED, True)		#turn on all LEDs required for zero 
-		
-		sleep(1)	
-		
-		for LED in LEDlist:
-			gpio.output(LED, False)		#turn off all LEDs required for zero
-		
-	def numOne():
-		#number one only needs one line on either side, either left or right
-		gpio.output(rightLED, True)
-		sleep(1)
-		gpio.output(rightLED, False)
-		
-	def ans():
-		#list of the functions 
-		fList = [numZero, numOne]
-		try:
-			for f in range(0, num):
-				fList[f]()
-		finally:
-			gpio.cleanup()
+    def __init__(self, num):
+        self.num = num
+        #the right LED is pin 8
+        #left is pin 7
+        #top is 11
+        #mid is 26
+        #bot is 40
 
+    def numZero(self):
+        # list of all lines of LED used to make zero
+        LEDList = [7, 8, 11, 40]
 
-ui = int(input("how much do you want to count up to? ")			
-a = Count(ui)
-print(a.ans())
+        for LED in LEDList:
+            gpio.output(LED, True)  # turn on all LEDs required for zero
+
+        sleep(1)
+
+        for LED in LEDList:
+            gpio.output(LED, False)  # turn off all LEDs required for zero
+
+    def numOne(self):
+        # number one only needs one line on either side, either left or right
+        gpio.output(8, True)
+        sleep(1)
+        gpio.output(8, False)
+
+    def ans(self):
+        # list of the functions
+        fList = [numZero, numOne]
+        try:
+            for f in range(0, self.num):
+                fList[f]()
+        finally:
+            gpio.cleanup()
+
+a = Count(2)
+a.ans()
